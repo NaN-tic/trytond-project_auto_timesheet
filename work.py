eb2e400d53c5825cf781a0307e3f810064f99d12
Bernat Brunet Torruella <bernat@nan-tic.com>
2013-12-17 10:50:26 +0100

    Control the duplication

--- work.py
+++ work.py
@@ -21,3 +21,25 @@ class Work(ModelSQL, ModelView):
                 [{'name': vl['timesheet_work_name']}])
             vl['work'] = timesheet_work[0].id
         return super(Work, cls).create(vlist)
+
+    @classmethod
+    def __setup__(cls):
+        super(Work, cls).__setup__()
+        cls._error_messages.update({
+                'to_many_duplicates': ('For this type of model you can only '
+                    'duplicate one by one.'),
+                })
+
+    @classmethod
+    def copy(cls, records, default=None):
+        if default is None:
+            default = {}
+        default = default.copy()
+        if len(records) > 1:
+            cls.raise_user_error('to_many_duplicates')
+        default.setdefault('work', None)
+        default.setdefault('timesheet_work_name',
+            "%s (copy)" % records[0].timesheet_work_name)
+        default.setdefault('product', records[0].product)
+        default.setdefault('party', records[0].party)
+        return super(Work, cls).copy(records, default=default)
