4fdeb93cc1853a6ac4f8957c427efa5c96c3ac8d
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-31 13:05:07 +0100

    Migrate to version 3.8

--- tests/test_project_auto_timesheet.py
+++ tests/test_project_auto_timesheet.py
@@ -3,27 +3,20 @@
 #copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
 from trytond.transaction import Transaction
 
 
-class ProjectAutoTimesheetTestCase(unittest.TestCase):
+class ProjectAutoTimesheetTestCase(ModuleTestCase):
     'Test ProjectAutoTimesheet module'
+    module = 'project_auto_timesheet'
 
     def setUp(self):
-        trytond.tests.test_tryton.install_module('project_auto_timesheet')
+        super(ProjectAutoTimesheetTestCase, self).setUp()
         self.project_work = POOL.get('project.work')
         self.company = POOL.get('company.company')
         self.user = POOL.get('res.user')
-
-    def test0005views(self):
-        'Test views'
-        test_view('project_auto_timesheet')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
 
     def test0010project(self):
         with Transaction().start(DB_NAME, USER, context=CONTEXT):
@@ -36,25 +29,25 @@ class ProjectAutoTimesheetTestCase(unittest.TestCase):
                     })
             new_context = self.user.get_preferences(context_only=True)
         with Transaction().start(DB_NAME, USER, context=new_context):
-            pwork1 = self.project_work(timesheet_work_name='test1')
+            pwork1 = self.project_work(name='test1')
             pwork1.save()
             self.assertEqual(pwork1.work.name, 'test1')
-            self.assertEqual(pwork1.timesheet_work_name, 'test1')
-            pwork2 = self.project_work(timesheet_work_name='test2')
+            self.assertEqual(pwork1.name, 'test1')
+            pwork2 = self.project_work(name='test2')
             pwork2.save()
             self.assertEqual(pwork2.work.name, 'test2')
-            self.assertEqual(pwork2.timesheet_work_name, 'test2')
+            self.assertEqual(pwork2.name, 'test2')
 
             work, = self.project_work.search([
-                    ('timesheet_work_name', '=', 'test1')])
+                    ('name', '=', 'test1')])
             self.assertEqual(work, pwork1)
 
             npwork1, npwork2 = self.project_work.copy([pwork1, pwork2])
             self.assertEqual(npwork1.work.name, 'test1')
-            self.assertEqual(npwork1.timesheet_work_name, 'test1')
+            self.assertEqual(npwork1.name, 'test1')
             self.assertNotEqual(npwork1.work, pwork1.work)
             self.assertEqual(npwork2.work.name, 'test2')
-            self.assertEqual(npwork2.timesheet_work_name, 'test2')
+            self.assertEqual(npwork2.name, 'test2')
             self.assertNotEqual(npwork2.work, pwork2.work)
 
 
--- work.py
+++ work.py
@@ -10,9 +10,6 @@ __metaclass__ = PoolMeta
 
 class Work:
     __name__ = 'project.work'
-    timesheet_work_name = fields.Function(fields.Char('Name', required=True),
-        'get_timesheet_work_name', searcher='search_timesheet_work_name',
-        setter='set_timesheet_work_name')
 
     @classmethod
     def create(cls, vlist):
@@ -23,27 +20,13 @@ class Work:
         for values in vlist:
             if 'work' not in values:
                 to_create.append({
-                        'name': values.get('timesheet_work_name'),
+                        'name': values.get('name'),
                         })
                 to_values.append(values)
         works = TimesheetWork.create(to_create)
         for values, work in izip(to_values, works):
             values['work'] = work.id
         return super(Work, cls).create(vlist)
-
-    def get_timesheet_work_name(self, name):
-        return self.work.name
-
-    @classmethod
-    def search_timesheet_work_name(cls, name, clause):
-        return [('work.name',) + tuple(clause[1:])]
-
-    @classmethod
-    def set_timesheet_work_name(cls, works, name, value):
-        Work = Pool().get('timesheet.work')
-        Work.write([p.work for p in works], {
-                'name': value,
-                })
 
     @classmethod
     def copy(cls, projects, default=None):
@@ -54,9 +37,10 @@ class Work:
         for project in projects:
             to_copy.append(project.work)
         works = TimesheetWork.copy(to_copy)
-        res = []
+        projects = super(Work, cls).copy(projects, default=default)
+        to_write = []
         for project, work in izip(projects, works):
-            d = default.copy()
-            d['work'] = work.id
-            res += super(Work, cls).copy([project], default=d)
-        return res
+            to_write.extend(([project], {'work': work.id}))
+        if to_write:
+            cls.write(*to_write)
+        return projects
