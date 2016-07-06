c190f7f4f48a7369737d249f5aaed5cb2a6431b4
Raimon Esteve <raimon@nan-tic.com>
2016-07-06 10:44:17 +0200

    FIx tests for 4.x series

--- tests/test_project_auto_timesheet.py
+++ tests/test_project_auto_timesheet.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
-import doctest
 import unittest
+from trytond.pool import Pool
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
-from trytond.pool import Pool
 
 from trytond.modules.company.tests import create_company, set_company
 
 
 class ProjectAutoTimesheetTestCase(ModuleTestCase):
-    'Test ProjectAutoTimesheet module'
+    'Test Project Auto Timesheet module'
     module = 'project_auto_timesheet'
 
     @with_transaction()
-    def test0010project(self):
-        pool = Pool()
-        ProjectWork = pool.get('project.work')
+    def test_project(self):
+        'Test project'
+        ProjectWork = Pool().get('project.work')
 
         company = create_company()
         with set_company(company):
@@ -30,7 +29,8 @@ class ProjectAutoTimesheetTestCase(ModuleTestCase):
             self.assertEqual(pwork2.work.name, 'test2')
             self.assertEqual(pwork2.name, 'test2')
 
-            work, = ProjectWork.search([('name', '=', 'test1')])
+            work, = ProjectWork.search([
+                    ('name', '=', 'test1')])
             self.assertEqual(work, pwork1)
 
             npwork1, npwork2 = ProjectWork.copy([pwork1, pwork2])
@@ -40,18 +40,10 @@ class ProjectAutoTimesheetTestCase(ModuleTestCase):
             self.assertEqual(npwork2.work.name, 'test2')
             self.assertEqual(npwork2.name, 'test2')
             self.assertNotEqual(npwork2.work, pwork2.work)
-            pwork3 = ProjectWork(name='test3', work=None)
-            pwork3.save()
-            self.assertEqual(pwork3.work.name, 'test3')
-            self.assertEqual(pwork3.name, 'test3')
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.company.tests import test_company
-    for test in test_company.suite():
-        if test not in suite:
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            ProjectAutoTimesheetTestCase))
+        ProjectAutoTimesheetTestCase))
     return suite
